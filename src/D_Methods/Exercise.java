e6511e4c2b0dd619165cae031863d66b754e4493
EduardoRomo1 <eromo0855@gmail.com>
2025-11-10 10:35:12 -0800

    Mulitplying Exercise

---
+++ src/D_Methods/Exercise.java
@@ -0,0 +1,14 @@
+package D_Methods;
+
+public class Exercise {
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        // Test the method
+        int result = multiply(5, 3);
+        System.out.println("The result is: " + result);
+    }
+}
